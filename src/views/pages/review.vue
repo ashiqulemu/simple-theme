8112357b965b6c80e03fd780031626c7e887cd2c
shaumik <shaumik.gh@gmail.com>
2020-01-26 15:36:33 +0600

    review page

--- src/views/pages/review.vue
+++ src/views/pages/review.vue
@@ -1,6 +1,28 @@
 <template>
     <div>
-        i am review page
+        <div class="review-header">
+            <div class="main-container">
+                <div class="review-title"><p class="review-title-p">Review</p></div>
+                <div class="review-sub-title">
+                    <a href="#" class="url-for-review">Home</a> <span class="slash">/</span> <a class="url-for-review" href="#">Review</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="review-block">
+            <div class="main-container">
+                <div class="single-employee-info-area">
+                    <div class="employee-image-area">
+                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRk8EgNijb2bZxPkl5Y0K3rVmb0r5maGFhXBd2nehQ0MA0TxObZ" alt="employee image" class="employee-image"/>
+                    </div>
+                    <div class="employee-name-area">
+                        <h5 class="employee-name">John Martin</h5>
+                    </div>
+                    <div class="employee-rating"></div>
+                    <div class="employee-details-area"></div>
+                </div>
+            </div>
+        </div>
 
     </div>
 </template>
