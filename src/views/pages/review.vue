358bf20fd53aca4a66de2b06bbc611c8bf75bc8e
shaumik <shaumik.gh@gmail.com>
2020-01-26 17:31:41 +0600

    review page

--- src/views/pages/review.vue
+++ src/views/pages/review.vue
@@ -1,25 +1,164 @@
 <template>
     <div>
         <div class="review-header">
-            <div class="main-container">
-                <div class="review-title"><p class="review-title-p">Review</p></div>
+            <div class="container">
+                <div class="review-title">
+                    <p class="review-title-p">Review</p>
+                </div>
                 <div class="review-sub-title">
-                    <a href="#" class="url-for-review">Home</a> <span class="slash">/</span> <a class="url-for-review" href="#">Review</a>
+                    <a href="#" class="url-for-review">Home</a>
+                    <span class="slash"> / </span>
+                    <a class="url-for-review" href="#">Review</a>
                 </div>
             </div>
         </div>
 
-        <div class="review-block">
-            <div class="main-container">
-                <div class="single-employee-info-area">
-                    <div class="employee-image-area">
-                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRk8EgNijb2bZxPkl5Y0K3rVmb0r5maGFhXBd2nehQ0MA0TxObZ" alt="employee image" class="employee-image"/>
+        <div class="review-area">
+            <div class="container">
+                <div class="row">
+                    <div class="single-employee-info-area col-md-4">
+                        <div class="review-container">
+                            <div class="employee-image-area minimum-margin-for-profile-details">
+                                <img src="../../assets/images/person/image1.jpg"
+                                     alt="employee image"
+                                     class="employee-image"/>
+                            </div>
+                            <div class="employee-name-area minimum-margin-for-profile-details">
+                                <h5>John Martin</h5>
+                            </div>
+                            <div class="employee-rating minimum-margin-for-profile-details">
+                                <i class="mdi mdi-star"></i>
+                                <i class="mdi mdi-star"></i>
+                                <i class="mdi mdi-star"></i>
+                                <i class="mdi mdi-star"></i>
+                                <i class="mdi mdi-star"></i>
+                            </div>
+                            <div class="employee-details-area minimum-margin-for-profile-details">
+                                <p>Lorem Ipsum Is Simply Dummy Text Of The Printing And Typesetting Industry. Lorem
+                                    Ipsum Has Been The Industry's Standard Dummy Text Ever Since The 1500s
+                                </p>
+                            </div>
+                        </div>
                     </div>
-                    <div class="employee-name-area">
-                        <h5 class="employee-name">John Martin</h5>
+                    <div class="single-employee-info-area col-md-4">
+                        <div class="review-container">
+                            <div class="employee-image-area minimum-margin-for-profile-details">
+                                <img src="../../assets/images/person/image2.jpg"
+                                     alt="employee image"
+                                     class="employee-image"/>
+                            </div>
+                            <div class="employee-name-area minimum-margin-for-profile-details">
+                                <h5>John Martin</h5>
+                            </div>
+                            <div class="employee-rating minimum-margin-for-profile-details">
+                                <i class="mdi mdi-star"></i>
+                                <i class="mdi mdi-star"></i>
+                                <i class="mdi mdi-star"></i>
+                                <i class="mdi mdi-star"></i>
+                                <i class="mdi mdi-star"></i>
+                            </div>
+                            <div class="employee-details-area minimum-margin-for-profile-details">
+                                <p>Lorem Ipsum Is Simply Dummy Text Of The Printing And Typesetting Industry. Lorem
+                                    Ipsum Has Been The Industry's Standard Dummy Text Ever Since The 1500s</p>
+                            </div>
+                        </div>
                     </div>
-                    <div class="employee-rating"></div>
-                    <div class="employee-details-area"></div>
+                    <div class="single-employee-info-area col-md-4">
+                        <div class="review-container">
+                            <div class="employee-image-area minimum-margin-for-profile-details">
+                                <img src="../../assets/images/person/image3.jpg"
+                                     alt="employee image"
+                                     class="employee-image"/>
+                            </div>
+                            <div class="employee-name-area minimum-margin-for-profile-details">
+                                <h5>John Martin</h5>
+                            </div>
+                            <div class="employee-rating minimum-margin-for-profile-details">
+                                <i class="mdi mdi-star"></i>
+                                <i class="mdi mdi-star"></i>
+                                <i class="mdi mdi-star"></i>
+                                <i class="mdi mdi-star"></i>
+                                <i class="mdi mdi-star"></i>
+                            </div>
+                            <div class="employee-details-area minimum-margin-for-profile-details">
+                                <p>Lorem Ipsum Is Simply Dummy Text Of The Printing And Typesetting Industry.
+                                    Lorem
+                                    Ipsum Has Been The Industry's Standard Dummy Text Ever Since The 1500s</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="single-employee-info-area col-md-4">
+                        <div class="review-container">
+                            <div class="employee-image-area minimum-margin-for-profile-details">
+                                <img src="../../assets/images/person/image4.jpg"
+                                     alt="employee image"
+                                     class="employee-image"/>
+                            </div>
+                            <div class="employee-name-area minimum-margin-for-profile-details">
+                                <h5>John Martin</h5>
+                            </div>
+                            <div class="employee-rating minimum-margin-for-profile-details">
+                                <i class="mdi mdi-star"></i>
+                                <i class="mdi mdi-star"></i>
+                                <i class="mdi mdi-star"></i>
+                                <i class="mdi mdi-star"></i>
+                                <i class="mdi mdi-star"></i>
+                            </div>
+                            <div class="employee-details-area minimum-margin-for-profile-details">
+                                <p>Lorem Ipsum Is Simply Dummy Text Of The Printing And Typesetting Industry. Lorem
+                                    Ipsum Has Been The Industry's Standard Dummy Text Ever Since The 1500s
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="single-employee-info-area col-md-4">
+                        <div class="review-container">
+                            <div class="employee-image-area minimum-margin-for-profile-details">
+                                <img src="../../assets/images/person/image5.jpg"
+                                     alt="employee image"
+                                     class="employee-image"/>
+                            </div>
+                            <div class="employee-name-area minimum-margin-for-profile-details">
+                                <h5>John Martin</h5>
+                            </div>
+                            <div class="employee-rating minimum-margin-for-profile-details">
+                                <i class="mdi mdi-star"></i>
+                                <i class="mdi mdi-star"></i>
+                                <i class="mdi mdi-star"></i>
+                                <i class="mdi mdi-star"></i>
+                                <i class="mdi mdi-star"></i>
+                            </div>
+                            <div class="employee-details-area minimum-margin-for-profile-details">
+                                <p>Lorem Ipsum Is Simply Dummy Text Of The Printing And Typesetting Industry. Lorem
+                                    Ipsum Has Been The Industry's Standard Dummy Text Ever Since The 1500s
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="single-employee-info-area col-md-4">
+                        <div class="review-container">
+                            <div class="employee-image-area minimum-margin-for-profile-details">
+                                <img src="../../assets/images/person/image6.jpg"
+                                     alt="employee image"
+                                     class="employee-image"/>
+                            </div>
+                            <div class="employee-name-area minimum-margin-for-profile-details">
+                                <h5>John Martin</h5>
+                            </div>
+                            <div class="employee-rating minimum-margin-for-profile-details">
+                                <i class="mdi mdi-star"></i>
+                                <i class="mdi mdi-star"></i>
+                                <i class="mdi mdi-star"></i>
+                                <i class="mdi mdi-star"></i>
+                                <i class="mdi mdi-star"></i>
+                            </div>
+                            <div class="employee-details-area minimum-margin-for-profile-details">
+                                <p>Lorem Ipsum Is Simply Dummy Text Of The Printing And Typesetting Industry. Lorem
+                                    Ipsum Has Been The Industry's Standard Dummy Text Ever Since The 1500s
+                                </p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
