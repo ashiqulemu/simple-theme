877d3e71ca28a16025d07a29c68e17649e9d9f69
hasib <hasib.arman.08@gmail.com>
2020-01-26 13:32:05 +0600

    pages

---
+++ src/views/pages/sign-in.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="container">
+        <div class="card">
+
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "sign-in"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/pages/thank-you.vue
@@ -0,0 +1,13 @@
+<template>
+    
+</template>
+
+<script>
+    export default {
+        name: "thank-you"
+    }
+</script>
+
+<style scoped>
+
+</style>
