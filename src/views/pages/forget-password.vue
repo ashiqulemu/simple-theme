b9cd931e840d7e6b73a1ed7bca2fb051369580b6
hasib <hasib.arman.08@gmail.com>
2020-01-26 16:55:54 +0600

    sign-in sign up forgot-password

--- src/views/pages/forget-password.vue
+++ src/views/pages/forget-password.vue
@@ -1,13 +1,76 @@
 <template>
-    
+    <div class="auth-form">
+        <div class="container-box">
+            <div class="md-title">
+                Reset Your Password
+            </div>
+            <div class="card">
+                <div class="input-group flex-nowrap">
+                    <div class="input-group-prepend">
+                        <span class="input-group-text">
+                            <i class="mdi mdi-email"></i>
+                        </span>
+                    </div>
+                    <input type="text"
+                           class="form-control"
+                           placeholder="Your Email"
+                           aria-label="User email">
+                </div>
+
+                <div class="text-center">
+                    <button type="button"
+                            class="btn full-btn">
+                        Send
+                    </button>
+                </div>
+                <div class="resend">
+                    IF DON'T RECEIVE Token?
+                    <button type="button"
+                            class="btn">
+                        Resend
+                    </button>
+                </div>
+
+                <div class="input-group flex-nowrap token-input">
+                   <input type="password"
+                           class="form-control "
+                           placeholder="Enter Token"
+                           aria-label="token">
+                </div>
+
+                <div class="input-group flex-nowrap">
+                    <div class="input-group-prepend">
+                        <span class="input-group-text">
+                            <i class="mdi mdi-lock"></i>
+                        </span>
+                    </div>
+                    <input type="password"
+                           class="form-control"
+                           placeholder="Password"
+                           aria-label="Password">
+                </div>
+                <div class="input-group flex-nowrap">
+                    <div class="input-group-prepend">
+                        <span class="input-group-text">
+                            <i class="mdi mdi-lock"></i>
+                        </span>
+                    </div>
+                    <input type="password"
+                           class="form-control"
+                           placeholder="Retype Password"
+                           aria-label="Retype Password">
+                </div>
+
+                <div class="text-center">
+                    <button type="button"
+                            class="btn auth-btn btn-md">
+                        Done
+                    </button>
+                </div>
+
+
+            </div>
+        </div>
+
+    </div>
 </template>
-
-<script>
-    export default {
-        name: "forget-password"
-    }
-</script>
-
-<style scoped>
-
-</style>
