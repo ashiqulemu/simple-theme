5c86224ecc5aee682069bdf7639b2e4b2b7e694b
ashiqulemu <ashiqulemu.jpi@gmail.com>
2020-01-27 14:39:35 +0600

    adding logo in project

--- src/components/partial/myHeader.vue
+++ src/components/partial/myHeader.vue
@@ -1,80 +1,105 @@
 <template>
-    <header class="header">
-       <div class="container">
-           <nav class="navbar navbar-expand-lg">
-               <a class="navbar-brand" href="#">
-                   <img src="../../assets/images/logo.png" height="30" alt="">
-               </a>
-               <button class="navbar-toggler user-defined" type="button" data-toggle="collapse" data-target="#navbarSupportedContent-4" aria-controls="navbarSupportedContent-4" aria-expanded="false" aria-label="Toggle navigation">
-                   <span class="mdi mdi-menu"></span>
-               </button>
-               <div class="collapse navbar-collapse nav-user-define" id="navbarSupportedContent-4">
-                   <ul class="navbar-nav ml-auto">
-                       <li class="nav-item">
-                           <a class="nav-link active" href="#">
-                                home <span class="sr-only">(current)</span>
-                           </a>
-                       </li>
-                       <li class="nav-item">
-                           <a class="nav-link" href="#">
-                               about
-                           </a>
-                       </li>
-                       <li class="nav-item">
-                           <a class="nav-link" href="#">
-                               feature
-                           </a>
-                       </li>
-                       <li class="nav-item">
-                           <a class="nav-link" href="#">
-                               screenshot
-                           </a>
-                       </li>
-                       <li class="nav-item">
-                           <a class="nav-link" href="#">
-                               team
-                           </a>
-                       </li>
-                       <li class="nav-item dropdown">
-                           <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink-5"
-                              data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                               <i class="fa fa-newspaper-o"></i> blog </a>
-                           <div class="dropdown-menu dropdown-menu-right dropdown-user-define" aria-labelledby="navbarDropdownMenuLink-4">
-                               <a class="dropdown-item" href="#">blog </a>
-                               <a class="dropdown-item" href="#">blog list</a>
-                               <a class="dropdown-item" href="#">blog list</a>
-                               <a class="dropdown-item" href="#">blog details</a>
-                               <a class="dropdown-item" href="#">left sidebar  </a>
-                               <a class="dropdown-item" href="#">right sidebar</a>
-                               <a class="dropdown-item" href="#">details left sidebar </a>
-                               <a class="dropdown-item" href="#">details right sidebar</a>
-                           </div>
-                       </li>
-                       <li class="nav-item">
-                           <a class="nav-link" href="#">
-                               price
-                           </a>
-                       </li>
-                       <li class="nav-item">
-                           <a class="nav-link" href="#">
-                               contact us
-                           </a>
-                       </li>
+   <div>
+       <header class="header">
+           <div class="container">
+               <nav class="navbar navbar-expand-lg">
+                   <a class="navbar-brand" href="#">
+                       <img src="../../assets/images/logo.png" height="30" alt="">
+                   </a>
+                   <button class="navbar-toggler user-defined" type="button" data-toggle="collapse" data-target="#navbarSupportedContent-4" aria-controls="navbarSupportedContent-4" aria-expanded="false" aria-label="Toggle navigation">
+                       <span class="mdi mdi-menu"></span>
+                   </button>
+                   <div class="collapse navbar-collapse nav-user-define" id="navbarSupportedContent-4">
+                       <ul class="navbar-nav ml-auto">
+                           <li class="nav-item">
+                               <a class="nav-link active" href="#">
+                                   home
+                               </a>
+                           </li>
+                           <li class="nav-item">
+                               <a class="nav-link" href="#">
+                                   about
+                               </a>
+                           </li>
+                           <li class="nav-item">
+                               <a class="nav-link" href="#">
+                                   feature
+                               </a>
+                           </li>
+                           <li class="nav-item">
+                               <a class="nav-link" href="#">
+                                   screenshot
+                               </a>
+                           </li>
+                           <li class="nav-item">
+                               <a class="nav-link" href="#">
+                                   team
+                               </a>
+                           </li>
+                           <li class="nav-item dropdown">
+                               <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink-5"
+                                  data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                   <i class="fa fa-newspaper-o"></i> blog </a>
+                               <div class="dropdown-menu dropdown-menu-right dropdown-user-define" aria-labelledby="navbarDropdownMenuLink-4">
+                                   <a class="dropdown-item" href="#">blog </a>
+                                   <a class="dropdown-item" href="#">blog list</a>
+                                   <a class="dropdown-item" href="#">blog list</a>
+                                   <a class="dropdown-item" href="#">blog details</a>
+                                   <a class="dropdown-item" href="#">left sidebar  </a>
+                                   <a class="dropdown-item" href="#">right sidebar</a>
+                                   <a class="dropdown-item" href="#">details left sidebar </a>
+                                   <a class="dropdown-item" href="#">details right sidebar</a>
+                               </div>
+                           </li>
+                           <li class="nav-item">
+                               <a class="nav-link" href="#">
+                                   price
+                               </a>
+                           </li>
+                           <li class="nav-item">
+                               <a class="nav-link" href="#">
+                                   contact us
+                               </a>
+                           </li>
 
-                       <li class="nav-item dropdown">
-                           <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink-4"
-                              data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                               other page </a>
-                           <div class="dropdown-menu dropdown-menu-right dropdown-user-define" aria-labelledby="navbarDropdownMenuLink-4">
-                               <a class="dropdown-item" href="#">Facebook</a>
-                               <a class="dropdown-item" href="#">Instagram</a>
-                           </div>
-                       </li>
-                   </ul>
-               </div>
-           </nav>
+                           <li class="nav-item dropdown">
+                               <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink-4"
+                                  data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                   other page </a>
+                               <div class="dropdown-menu dropdown-menu-right dropdown-user-define" aria-labelledby="navbarDropdownMenuLink-4">
+                                   <a class="dropdown-item" href="#">Facebook</a>
+                                   <a class="dropdown-item" href="#">Instagram</a>
+                               </div>
+                           </li>
+                       </ul>
+                   </div>
+               </nav>
+           </div>
+       </header>
+       <div class="home-slider">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-6 col-md-6 col-xs-12">
+                        <div class="left-content">
+
+                        </div>
+                    </div>
+                    <div class="col-lg-6 col-md-6 col-xs-12">
+                        <div class="right-content">
+                            <p class="title">big discount</p>
+                            <h1 class="text-center py-3">
+                                Make your own business with some smiley!</h1>
+
+                            <div class="button-action">
+                                <button type="button"> Get Your choice! </button>
+                                <button type="button"> Get Your choice! </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
        </div>
-    </header>
+   </div>
 </template>
 
 <script>
