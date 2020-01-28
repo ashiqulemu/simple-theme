92b223fb47894767fc5d559d8f44b4f4a703ceb9
hasib <hasib.arman.08@gmail.com>
2020-01-28 12:41:39 +0600

    sign-in sign up forgot-password

--- src/views/blog/blog-details.vue
+++ src/views/blog/blog-details.vue
@@ -185,13 +185,56 @@
                     </ul>
                 </nav>
             </div>
-            <div class="blog-sidebar">
+            <div class="blog-sidebar col-md-4">
                 <div class="input-group search-box">
                     <input class="form-control"
                            placeholder="Search"
                            type="text">
                     <div class="input-group-btn">
-                        <button class="btn btn-primary" type="submit"><i class="mdi mdi-magnify"></i></button>
+                        <button class="btn submit-btn" type="submit">
+                            <i class="mdi mdi-magnify"></i>
+                        </button>
+                    </div>
+                </div>
+                <div class="side-category">
+                    <div class="category-title">Categories</div>
+                    <ul class="nav navbar-nav category-list">
+                        <li>
+                            <a href="#" data-toggle="collapse"
+                               data-target="#submenu-1"
+                               class="category-item"
+                            >
+                                <span>Category 1</span>
+                                <i class="mdi mdi-chevron-right"></i>
+                            </a>
+                            <ul id="submenu-1" class="collapse">
+                                <li><a href="#"><i class="fa fa-angle-double-right"></i> SUBMENU 1.1</a></li>
+                                <li><a href="#"><i class="fa fa-angle-double-right"></i> SUBMENU 1.2</a></li>
+                                <li><a href="#"><i class="fa fa-angle-double-right"></i> SUBMENU 1.3</a></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="#"
+                               class="category-item"
+                            >
+                                <span>Category 1</span>
+                                <i class="mdi mdi-chevron-right"></i>
+                            </a>
+                        </li>
+                        <li>
+                           <a href="#"
+                               class="category-item"
+                            >
+                                <span>Category 1</span>
+                                <i class="mdi mdi-chevron-right"></i>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="recent-post">
+                    <div class="post-title">Categories</div>
+                    <div>
+
                     </div>
                 </div>
             </div>
