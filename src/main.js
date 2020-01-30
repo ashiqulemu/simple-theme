98b8d65fef04dae921770095f0a7d71c1fba2e92
hasib <hasib.arman.08@gmail.com>
2020-01-30 17:47:38 +0600

    footer

--- src/main.js
+++ src/main.js
@@ -13,20 +13,6 @@ import 'vue-slick/src/slickCarousel'
 
 Vue.config.productionTip = false;
 
-Vue.filter('truncate', function (value, limit) {
-    if (value.length > limit) {
-        value = value.substring(0, (limit - 3)) + '...';
-    }
-
-    return value
-})
-Vue.filter('two_digits', function (value) {
-    if(value.toString().length <= 1)
-    {
-        return "0"+value.toString();
-    }
-    return value.toString();
-});
 
 new Vue({
     router,
