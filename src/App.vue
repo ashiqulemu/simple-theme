11221d32d4f374326f8c3560a93931312573873d
Jahurul Islam <43181519+Jahurulislamjl@users.noreply.github.com>
2020-02-01 17:37:56 +0600

    top bar

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
     <div id="app">
-        <top-bar/>
         <my-header/>
         <router-view/>
         <my-footer/>
@@ -8,13 +7,12 @@
 </template>
 
 <script>
-    import topBar from "./components/partial/topBar";
     import myHeader from "./components/partial/myHeader";
     import myFooter from "./components/partial/myFooter"
 
     export default {
         components: {
-            myFooter, myHeader, topBar
+            myFooter, myHeader,
         }
     }
 </script>
--- src/components/partial/myHeader.vue
+++ src/components/partial/myHeader.vue
@@ -1,6 +1,35 @@
 <template>
     <div>
         <header class="header">
+            <div class="top-bar">
+                <div class="container">
+                    <nav class="navbar navbar-expand-lg navbar-dark">
+                        <div class="d-flex">
+                            <div class="contact d-flex">
+                                <p><i class="mdi mdi-phone"></i> +01934534534</p>
+                            </div>
+
+                            <div>
+                                <ul class="navbar-nav">
+                                    <li class="nav-item">
+                                        <a href="" class="nav-link">Link1</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a href="" class="nav-link">Link2</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a href="" class="nav-link">Link3</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a href="" class="nav-link">Link4</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+
+                    </nav>
+                </div>
+            </div>
             <div class="container">
                 <nav class="navbar navbar-expand-lg">
                     <a class="navbar-brand" href="#">
