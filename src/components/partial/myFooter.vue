612d37bd1d051e518720dd0c3e1666d14cc84b48
jahurul <jahurulislam.soft@gmail.com>
2020-02-01 14:59:55 +0600

    footer

--- src/components/partial/myFooter.vue
+++ src/components/partial/myFooter.vue
@@ -1,9 +1,9 @@
 <template>
-    <div class="footer">
+    <div class="footer p-5">
         <div class="container">
-            <div class="row py-3">
+            <div class="row">
 
-                <div class="footer-top">
+                <div class="col-12 footer-top d-flex">
                     <div>
                         <p>
                             BE THE FIRS TO KNOW
@@ -14,20 +14,92 @@
                             Sign up for newsletter today.
                         </P>
                     </div>
-                    <div></div>
-                    <div></div>
+                    <div>
+                        <input type="text" placeholder="Email Address">
+                        <input type="button" value="Subscribe">
+                    </div>
+                    <div class="media-icon">
+                        <button class="mdi mdi-facebook icon"></button>
+                        <button class="mdi mdi-twitter icon"></button>
+                        <button class="mdi mdi-linkedin icon"></button>
+                        <button class="mdi mdi-github-circle icon"></button>
+                    </div>
                 </div>
 
-                <div class="footer-header">
+                <div class="col-12 footer-header mt-3">
+                    <div class="row">
+                        <div class="col-md-4">
+                            <p>CONTACT INFORMATION</p>
+                            <div>
+                                <div>
+                                    <p>ADDRESS</p>
+                                    <p>jashore</p>
+                                </div>
+                                <div>
+                                    <p>PHONE</p>
+                                    <p>01956767686</p>
+                                </div>
+                                <div>
+                                    <p>EMAIL</p>
+                                    <p>jashore@gamil.com</p>
+                                </div>
+                            </div>
+                        </div>
 
-                </div>
+                        <div class="col-md-8">
+                            <div class="row">
+                                <div class="col-12 pb-2">
+                                    <div class="row info-section d-flex">
+                                        <div>
+                                            <p>MY ACCOUNT</p>
+                                            <div class="d-flex information">
+                                                <div class="d-flex flex-column">
+                                                    <a href=""> About Us</a>
+                                                    <a href=""> Contact Us</a>
+                                                    <a href=""> My Account</a>
+                                                </div>
+                                                <div class="d-flex flex-column info-right">
+                                                    <a href=""> Order History</a>
+                                                    <a href=""> Advanced Search</a>
+                                                    <a href=""> Login</a>
+                                                </div>
+                                            </div>
+                                        </div>
 
-                <div class="col-12 company-media mt-1">
-                    <div class="icon-section d-flex py-2">
-                        <button class="mdi mdi-facebook media-icon"></button>
-                        <button class="mdi mdi-twitter media-icon"></button>
-                        <button class="mdi mdi-linkedin media-icon"></button>
-                        <button class="mdi mdi-github-circle media-icon"></button>
+                                        <div>
+                                            <p>MAIN FEATURE</p>
+                                            <div class="d-flex feature">
+                                                <div class="d-flex flex-column">
+                                                    <a href=""> Super First</a>
+                                                    <a href=""> Super First</a>
+                                                    <a href=""> Super First</a>
+                                                </div>
+                                                <div class="d-flex flex-column feature-right">
+                                                    <a href=""> Super First</a>
+                                                    <a href=""> Super First</a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="col-12 copy-right d-flex pt-2">
+                                    <div>
+                                        Copy@2020
+                                    </div>
+                                    <div>
+                                        Working hour
+                                    </div>
+
+                                    <div class="d-flex">
+                                        <div>card</div>
+                                        <div>card</div>
+                                        <div>card</div>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
                     </div>
                 </div>
 
