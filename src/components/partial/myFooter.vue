a2bb3ed4adbfb6852453a88b267842e21cb86351
jahurul <jahurulislam.soft@gmail.com>
2020-02-01 12:48:15 +0600

    Content editing

--- src/components/partial/myFooter.vue
+++ src/components/partial/myFooter.vue
@@ -1,37 +1,51 @@
 <template>
-    <div class="bg-primary">
+    <div class="footer-content">
         <div class="container">
-        <div class="row py-3 footer-content">
+            <div class="row py-3">
 
-            <div class="col-md-6 company-contact">
-                <div class="d-flex">
-                    <i class="mdi mdi-map-marker contact-icon"></i>
-                    <p>Jashore, Khulna, Bangladesh</p>
+                <div class="col-md-4 company-contact">
+                    <p class="lead">Contact</p>
+                    <div class="d-flex">
+                        <i class="mdi mdi-map-marker contact-icon"></i>
+                        <p class="info">Jashore, Khulna, Bangladesh</p>
+                    </div>
+                    <div class="d-flex">
+                        <i class="mdi mdi-phone contact-icon"></i>
+                        <p class="info">0098230483242</p>
+                    </div>
+                    <div class="d-flex">
+                        <i class="mdi mdi-email contact-icon"></i>
+                        <p class="info">mdit@gmail.com</p>
+                    </div>
                 </div>
-                <div class="d-flex">
-                    <i class="mdi mdi-phone contact-icon"></i>
-                    <p>0098230483242</p>
-                </div>
-                <div class="d-flex">
-                    <i class="mdi mdi-email contact-icon"></i>
-                    <p>mdit@gmail.com</p>
-                </div>
-            </div>
 
-            <div class="col-md-6 company-media">
-                <div class="lead">About The Company</div>
-                <div>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi?</p>
+                <div class="col-md-4 information">
+                    <div class="lead">Information</div>
+                    <div class="info-link d-flex flex-column mt-2">
+                        <a href="">About Us</a>
+                        <a href="">Blog</a>
+                        <a href="">Faq</a>
+                    </div>
                 </div>
-                <div class="icon-section py-2">
-                    <button class="mdi mdi-facebook media-icon"></button>
-                    <button class="mdi mdi-twitter media-icon"></button>
-                    <button class="mdi mdi-linkedin media-icon"></button>
-                    <button class="mdi mdi-github-circle media-icon"></button>
+
+                <div class="col-md-4">
+                    <div class="lead">Support</div>
+                    <div>
+
+                    </div>
                 </div>
+
+                <div class="col-12 company-media mt-1">
+                    <div class="icon-section d-flex py-2">
+                        <button class="mdi mdi-facebook media-icon"></button>
+                        <button class="mdi mdi-twitter media-icon"></button>
+                        <button class="mdi mdi-linkedin media-icon"></button>
+                        <button class="mdi mdi-github-circle media-icon"></button>
+                    </div>
+                </div>
+
             </div>
         </div>
-    </div>
     </div>
 
 </template>
