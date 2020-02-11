12ef537bbc6acfa74b9b4e0ebf2d9bbac2945897
hasib <hasib.arman.08@gmail.com>
2020-02-11 12:03:34 +0600

    breadcumb

--- src/views/pages/thank-you.vue
+++ src/views/pages/thank-you.vue
@@ -1,6 +1,10 @@
 <template>
  <section class="thank-you">
-     <div class="container pt-5">
+     <div class="container">
+         <ol class="breadcrumb my-breadcrumb">
+                <li>Other Page</li>
+                <li>Thank You</li>
+            </ol>
          <div class="inner">
              <h2 class="text-uppercase text-primary">Welcome to micro theme </h2>
              <p class="text-capitalize">you can create your own professional website using our theme , it will be 100% responsive also you can use single page application that will be your page not refreshed , if you have any suggestion or any guide lines we are waiting to hear from you </p>
